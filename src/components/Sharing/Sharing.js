226ec87666e401dccf1a3a4b7eee3c3d4f1394e0
Bloodbee <mathieu_dufour@hotmail.fr>
2019-01-27 21:56:52 +1100

    fix miswording for sharing scss module

--- src/components/Sharing/Sharing.js
+++ src/components/Sharing/Sharing.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import { Link } from 'gatsby';
-import styles from './sharing.module.scss';
+import styles from './Sharing.module.scss';
 
 const cx = classNames.bind(styles);
 
