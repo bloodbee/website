261b0d3dcc8a09fccaa52f4a7f0cd0d8cfca76d8
Bloodbee <mathieu_dufour@hotmail.fr>
2019-02-09 15:02:02 +1030

    fix contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import styles from './Contact.module.scss';
 
-const Contact = () => return (
-  <form name="contact" className={styles['contact']} data-netlify="true">
-    <p>
-      <label>Name</label>
-      <input type="text" name="name" />
-    </p>
-    <p>
-      <label>Email</label>
-      <input type="email" name="email" />
-    </p>
-    <p>
-      <label>Your message</label>
-      <textarea name="message"></textarea>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
-);
+const Contact = () => {
+  return (
+    <form name="contact" className={styles['contact']} data-netlify="true">
+      <p>
+        <label>Name</label>
+        <input type="text" name="name" />
+      </p>
+      <p>
+        <label>Email</label>
+        <input type="email" name="email" />
+      </p>
+      <p>
+        <label>Your message</label>
+        <textarea name="message"></textarea>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  );
+};
 
 export default Contact;
