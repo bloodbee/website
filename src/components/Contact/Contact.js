f67e5ec8d8de43460210076c44fbcb785c7bc764
Bloodbee <mathieu_dufour@hotmail.fr>
2019-02-09 18:05:34 +1030

    add honeypot and fix form contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import styles from './Contact.module.scss';
 
-const Contact = () => {
-  return (
-    <form name="contact" className={styles['contact']} data-netlify="true" data-netlify-honeypot="bot-field">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label>Name</label>
-        <input type="text" name="name" />
-      </p>
-      <p>
-        <label>Email</label>
-        <input type="email" name="email" />
-      </p>
-      <p>
-        <label>Your message</label>
-        <textarea name="message"></textarea>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-  );
-};
+const Contact = () => (
+  <form name="contact" method="post" className={styles['contact']} data-netlify="true" data-netlify-honeypot="bot-field">
+    <input type="hidden" name="form-name" value="contact" />
+    <p className={styles['hidden']}>
+      <label>
+        Don’t fill this out:{" "}
+        <input name="bot-field" />
+      </label>
+    </p>
+    <p>
+      <label>Name</label>
+      <input type="text" name="name" />
+    </p>
+    <p>
+      <label>Email</label>
+      <input type="email" name="email" />
+    </p>
+    <p>
+      <label>Your message</label>
+      <textarea name="message"></textarea>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
+);
 
 export default Contact;
