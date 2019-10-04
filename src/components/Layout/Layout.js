b5255f7862e9773a320ae8666dd035c413d2e7b5
mdufour <mathieu.dufour@whereyoulove.com>
2019-10-04 12:51:44 +0200

    remove that shit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -12,20 +12,6 @@ const Layout = ({ children, title, description }) => (
       <meta name="description" content={description} />
       <meta name="keywords" content="space, web, developer, cryptocurrency, entrepreneur, css, js, html, react, reactjs, develomment, www, backend, frontend, bloodbee, side, project, cryptocurrencies, blockchain, php, laravel, nodejs, python, ai, machine, learning, artificial, intelligence" />
       <meta name="google-site-verification" content="nm1U43c4PkTklri8nelGNn_vD_M4SoDW61zLuD2oWxc" />
-      <script type="application/ld+json">
-      {
-        "@context": "http://schema.org",
-        "@type": "Organization",
-        "name": "Bloodbee",
-        "url": "https://bloodbee.space",
-        "address": "Montpellier, France",
-        "sameAs": [
-          "https://www.facebook.com/mathieudufour.fr",
-          "https://twitter.com/Mathieu_Dufour",
-          "https://www.linkedin.com/in/dufourmathieu"
-        ]
-      }
-    </script>
     </Helmet>
     {children}
   </div>
